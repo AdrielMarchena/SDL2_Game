a0d71b5f8c2dc9bebf7ab17996e25b401390ce37
AdrielMarchena <Adriel_contato@hotmail.com>
2020-10-23 12:15:42 -0300

    Overloading operators

--- src/Engine/Math.h
+++ src/Engine/Math.h
@@ -20,6 +20,81 @@ namespace Engine
 			}
 
 			float x, y;
+
+			//Override +
+			Vec2f operator+(const Vec2f& b)
+			{
+				Vec2f a;
+				a.x = this->x + b.x;
+				a.y = this->y + b.y;
+				return a;
+			}
+			//Override -
+			Vec2f operator-(const Vec2f& b)
+			{
+				Vec2f a;
+				a.x = this->x - b.x;
+				a.y = this->y - b.y;
+				return a;
+			}
+			//Override *
+			Vec2f operator*(const Vec2f& b)
+			{
+				Vec2f a;
+				a.x = this->x * b.x;
+				a.y = this->y * b.y;
+				return a;
+			}
+			//Override /
+			Vec2f operator/(const Vec2f& b)
+			{
+				Vec2f a;
+				a.x = this->x / b.x;
+				a.y = this->y / b.y;
+				return a;
+			}
+			//Override +=
+			Vec2f& operator+=(const Vec2f& b)
+			{
+				this->x += b.x;
+				this->y += b.y;
+				return *this;
+			}
+			//Override -=
+			Vec2f& operator-=(const Vec2f& b)
+			{
+				this->x -= b.x;
+				this->y -= b.y;
+				return *this;
+			}
+			//Override *=
+			Vec2f& operator*=(const Vec2f& b)
+			{
+				this->x *= b.x;
+				this->y *= b.y;
+				return *this;
+			}
+			//Override /=
+			Vec2f& operator/=(const Vec2f& b)
+			{
+				this->x /= b.x;
+				this->y /= b.y;
+				return *this;
+			}
+			//Override == Identical
+			bool operator==(const Vec2f& b)
+			{
+				if (this->x == b.x && this->y == b.y)
+					return true;
+				return false;
+			}
+			//Override != Identical
+			bool operator!=(const Vec2f& b)
+			{
+				if (this->x == b.x || this->y == b.y)
+					return false;
+				return true;
+			}
 		};
 	}
 }
